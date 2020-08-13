6bcaf5ccc73bbdb1e35623e029240e8f043c1e97
ChinaNuke <chinanuke@nuke666.cn>
2020-08-13 13:19:50 +0800

    修改了mysh的readline部分使其能够在Windows上编译通过

--- myshell/myshell.c
+++ myshell/myshell.c
@@ -121,22 +121,46 @@ int mysh_execute(char **args) {
     return mysh_launch(args);
 }
 
+#define LSH_RL_BUFSIZE 1024
 /**
    @brief 从标准输入读入一行
    @return 读入的行
  */
 char *mysh_read_line(void) {
-    char *line = NULL;
-    size_t bufsize = 0;  // 让 getline 帮我们分配缓冲区
-    if (getline(&line, &bufsize, stdin) == -1) {
-        if (feof(stdin)) {
-            exit(EXIT_SUCCESS);  // 用户输入了 EOF
+    int bufsize = LSH_RL_BUFSIZE;
+    int position = 0;
+    char *buffer = malloc(sizeof(char) * bufsize);
+    int c;
+
+    if (!buffer) {
+        fprintf(stderr, "mysh: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    while (1) {
+        // Read a character
+        c = getchar();
+
+        if (c == EOF) {
+            exit(EXIT_SUCCESS);
+        } else if (c == '\n') {
+            buffer[position] = '\0';
+            return buffer;
         } else {
-            perror("mysh: getline\n");
-            exit(EXIT_FAILURE);
+            buffer[position] = c;
+        }
+        position++;
+
+        // If we have exceeded the buffer, reallocate.
+        if (position >= bufsize) {
+            bufsize += LSH_RL_BUFSIZE;
+            buffer = realloc(buffer, bufsize);
+            if (!buffer) {
+                fprintf(stderr, "lsh: allocation error\n");
+                exit(EXIT_FAILURE);
+            }
         }
     }
-    return line;
 }
 
 #define LSH_TOK_BUFSIZE 64
